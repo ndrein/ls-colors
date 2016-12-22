742b7f236ce872547c79ef40bddcd60e696fddeb
Noah Reinhardt <ndrein@gmail.com>
2016-12-22 16:50:33 -0500

    Don't need setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='ls-colors',
-      version='0.1',
-      description='Shell utility for colouring ls',
-      author='Noah Reinhardt',
-      author_email='ndrein@gmail.com',
-      packages=['ls_colors'],
-      scripts=['bin/ls'])
