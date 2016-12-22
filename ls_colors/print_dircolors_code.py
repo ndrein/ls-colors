7cad0eb3671eba44b49b8039fb9fb07bd950a88b
Noah Reinhardt <ndrein@gmail.com>
2016-12-22 18:24:17 -0500

    Refactor and document

--- ls_colors/print_dircolors_code.py
+++ ls_colors/print_dircolors_code.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """
 Modify system environment to colorize ls based on file extensions
+
+Call this script with the arguments to ls
+Eg. print_dircolors_code.py -al my/folder
+
+Conventions:
+dircolors - output from `dircolors -p` + our extensions
+dircolors_code - shell code needed to modify environment
 """
 
 from sys import argv
-from os import remove
 from tempfile import NamedTemporaryFile
 from subprocess import check_output, CalledProcessError
 
@@ -15,8 +21,8 @@ def format_ext(ext, color):
     """
     Format ext to add to `dircolors -p`
 
-    Eg.
-    .txt 00;38
+    >>> format_ext('txt', '33')
+    .txt 00:33\n
 
     :return: str
     """
@@ -25,33 +31,56 @@ def format_ext(ext, color):
 
 def get_dircolors(ext_to_color):
     """
+    Run `dircolors -p` and add the given extensions to the output
+
     :return: str
     """
     return check_output(['dircolors'] + ['-p']).decode('utf-8') + \
            ''.join(format_ext(ext, color) for ext, color in ext_to_color.items())
 
 
-def get_dircolors_from_file(filename):
-    output = check_output(['dircolors'] + ['-b'] + [filename]).decode('utf-8')
-    print(output)
+def get_dircolors_code_from_file(filename):
+    """
+    Get shell code by running `dircolors -b` on filename
+
+    :param filename: str
+    :return: shell code
+    """
+    return check_output(['dircolors'] + ['-b'] + [filename]).decode('utf-8')
 
 
-def run_dircolors(dircolors):
-    try:
-        tmp = NamedTemporaryFile(mode='w+t', delete=False)
-        tmp.write(dircolors)
-        tmp.close()
+def write_dircolors_to_file(dircolors):
+    """
+    :param dircolors: str
+    :return: filename
+    """
+    tmp = NamedTemporaryFile(mode='w+t', delete=False)
+    tmp.write(dircolors)
+    tmp.close()
 
-        get_dircolors_from_file(tmp.name)
+    return tmp.name
 
-        remove(tmp.name)
-    except FileNotFoundError:
-        pass
+
+def get_dircolors_code(dircolors):
+    """
+    :param dircolors: str
+    :return: shell code
+    """
+    file = write_dircolors_to_file(dircolors)
+
+    dircolors_code = get_dircolors_code_from_file(file)
+    return dircolors_code
 
 
 def print_dircolors_code(ls_args):
     ext_to_color = get_extension_to_color(ls_args)
-    run_dircolors(get_dircolors(ext_to_color))
+
+    # dircolors is the output of `dircolors -p` along with out custom extensions
+    dircolors = get_dircolors(ext_to_color)
+
+    # dircolors_code is shell code used to modify our environment
+    print(get_dircolors_code(dircolors))
+
 
 if __name__ == '__main__':
     try:
